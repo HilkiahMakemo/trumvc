13550532837f6088099d10bb89dd96a5e93b6d10
Hilkiah Makemo <hilmak01@gmail.com>
2018-02-02 22:31:33 -0700

    Modified system's autorun and init files

--- system/init.php
+++ system/init.php
@@ -1,38 +1,33 @@
 <?php
 
-$APP = TruMVC\Core\Application::instance();
+TruMVC\Core\Application::instance()
 
 
-$APP->set('blueprint', function(){
+->set('blueprint', function(){
     $bp = require _ROOT_ . 'blueprint.php';
     return json_decode( json_encode($bp) );
-
 })
+
+
 ->set('routing', function($app){
    $routes = $app->get('blueprint')->routes;
    return new TruMVC\Http\Routing($routes.DS);
 })
+
+
 ->set('request', function(){
     return TruMVC\Http\Request::fromGlobals();
-
 })
+
+
 ->set('response', function(){
     return new TruMVC\Http\Response();
-
 })
+
+
 ->set('config', function($app){
     $configs = $app->get('blueprint')->configs;
     return new TruMVC\Core\Configurator($configs);
-
-})
-->set('hilkiah', function(){
-    return 'Makemo';
-
-})
-->set('hilkiah', function(){
-    return 'Makemo';
 });
 
-dump($APP->get('blueprint'));
-
-return $APP;
+require SYSTEM . 'boot' . DS . 'autorun.php';
